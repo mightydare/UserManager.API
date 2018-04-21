6c053681a43741c939fc5df2803912861621a0ce
mightydare <darko.stojanov@yahoo.com>
2018-04-21 08:02:31 +0200

    Add Users

---
+++ UserManager.MVC/Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using UserManager.MVC.Models;
+
+namespace UserManager.MVC.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Users")]
+    public class UsersController : Controller
+    {
+        // GET: api/Users
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return new List<User>
+            {
+                new User("Ivan", "Stojanov")
+                {
+                    Age = 22,
+                    Index = 10,
+                }
+            };
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Users
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Users/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ UserManager.MVC/Models/User.cs
@@ -0,0 +1,21 @@
+﻿namespace UserManager.MVC.Models
+{
+    public class User
+    {
+        public User(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public string Name { get; }
+        public string Surname { get; }
+        public int Age { get; set; }
+        public int Index { get; set; }
+
+        public bool IsRetired()
+        {
+            return Age > 65;
+        }
+    }
+}
